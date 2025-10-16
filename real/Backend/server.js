093860edf6d0bafd4e8a49355256dbbc76dea5d3
Nashonlyy-dev <durgaysubedi3@gmail.com>
2025-10-16 10:13:08 +0630

    Update server.js

--- real/Backend/server.js
+++ real/Backend/server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 connectDB();
 
 
-app.listen(3000, ()=>{
-    console.log("Server is running at port 3000");
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
